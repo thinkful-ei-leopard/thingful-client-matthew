04407f3625314150a3c599b2341c7b71c16c97cd
Matthew Wagaman <averaqedev@gmail.com>
2020-03-06 12:01:48 -0800

    refactored for jwt

---
+++ src/services/auth-api-service.js
@@ -0,0 +1,19 @@
+import config from '../config';
+
+const AuthApiService = {
+  postLogin({user_name, password}) {
+    return fetch(`${config.API_ENDPOINT}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user_name, password })
+    })
+      .then(res => 
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()); 
+  },
+}
+
+export default AuthApiService;
--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -16,7 +16,7 @@ const ThingApiService = {
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
       headers: {
-        'Authorization': `Basic ${TokenService.getAuthToken()}`
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`
       },
     })
       .then(res =>
@@ -28,7 +28,7 @@ const ThingApiService = {
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
       headers: {
-        'Authorization': `Basic ${TokenService.getAuthToken()}`
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`
       },
     })
       .then(res =>
@@ -42,7 +42,7 @@ const ThingApiService = {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
-        'Authorization': `Basic ${TokenService.getAuthToken()}`
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`
       },
       body: JSON.stringify({
         thing_id: thingId,
